4fb46cceb4a5eb7876eb0ce3215749650abda317
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-31 16:56:38 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let n = word.length;
+  for (let i = 0; i < n / 2; i++) {
+      if (word.charAt(i) !== word.charAt(n - 1 - i)) {
+          return false;
+      }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
+
 */
 
 /*
   Add written explanation of your solution here
+  
+  `write a function that returns true or false and  also
+   The index of the first character is 0,
+    the second character is 1, and so on.`
 */
 
 // You can run `node index.js` to view these console logs
